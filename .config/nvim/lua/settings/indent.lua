52fc5d0e8e03cb7c523ccd09b774804ab6a4c425
barrydevp <barrydevp@gmail.com>
2021-06-20 11:02:06 +0700

    add auto suggestions

---
+++ .config/nvim/lua/settings/indent.lua
@@ -0,0 +1,5 @@
+vim.cmd([[autocmd Filetype python setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4]])
+vim.cmd([[autocmd Filetype go setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4]])
+vim.cmd([[autocmd Filetype javascript setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4]])
+vim.cmd([[autocmd Filetype lua setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4]])
+
