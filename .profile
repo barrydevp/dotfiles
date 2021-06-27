4787c188876c9ee3c38ed8173d3b740fa847d817
barrydevp <barrydevp@gmail.com>
2021-06-27 16:32:47 +0700

    linuxxxx

---
+++ .profile
@@ -0,0 +1,11 @@
+export EDITOR=/usr/bin/vim
+export QT_QPA_PLATFORMTHEME="qt5ct"
+export QT_AUTO_SCREEN_SCALE_FACTOR=0
+export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
+export GTK_IM_MODULE=ibus
+export QT_IM_MODULE=ibus
+export XMODIFIERS=@im=ibus
+export QT4_IM_MODULE=ibus
+export CLUTTER_IM_MODULE=ibus
+ibus-daemon -drx
+setxkbmap -option ctrl:nocaps
---
+++ openbox-pkg-install.sh
@@ -0,0 +1,6 @@
+yay -S rsync python psmisc xorg-xprop xorg-xwininfo imagemagick ffmpeg wireless_tools openbox \
+nitrogen dunst tint2 rofi \
+qt5-styleplugins lxsession rxvt-unicode xclip scrot thunar thunar-archive-plugin \
+thunar-volman ffmpegthumbnailer tumbler viewnior mpv mpd mpc ncmpcpp pavucontrol parcellite \
+neofetch w3m htop picom-git obmenu-generator gtk2-perl xsettingsd
+
