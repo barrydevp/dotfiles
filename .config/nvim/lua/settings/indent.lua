7c0df561efac3e807ea6a5638702011647aa507e
barrydevp <barrydevp@gmail.com>
2021-07-02 01:02:02 +0700

    fix fnm, push lsp package installed

--- .config/nvim/lua/settings/indent.lua
+++ .config/nvim/lua/settings/indent.lua
@@ -1,5 +1,5 @@
 vim.cmd([[autocmd Filetype python setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4]])
 vim.cmd([[autocmd Filetype go setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4]])
-vim.cmd([[autocmd Filetype javascript setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4]])
+vim.cmd([[autocmd Filetype javascript setlocal expandtab tabstop=2 shiftwidth=2 softtabstop=2]])
 vim.cmd([[autocmd Filetype lua setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4]])
 
