71ce0da4042222ff2cc6903816ac034cf3e5eac4
barrydevp <barrydevp@gmail.com>
2021-11-19 19:07:39 +0700

    nvim

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -37,7 +37,7 @@ dap.configurations.cpp = {
     --
     -- But you should be aware of the implications:
     -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
-    runInTerminal = true,
+    runInTerminal = false,
     -- pid = require('dap.utils').pick_process,
   },
 }
