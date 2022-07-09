5ce234d5ffcdda6ab8f1cd5b30bd72c73b103290
barrydevp <barrydevp@gmail.com>
2022-07-09 20:05:09 +0700

    nvim

---
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -0,0 +1,35 @@
+local ok, null_ls = pcall(require, "null-ls")
+
+if not ok then
+    return
+end
+
+local lsp_utils = require("plugins.lsp.utils")
+
+local builtins = null_ls.builtins
+
+local sources = {
+    -- Diagnostics
+    builtins.diagnostics.eslint_d,
+    builtins.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
+
+    -- Format
+    builtins.formatting.prettierd.with({ filetypes = { "html", "json", "markdown", "scss", "css" } }),
+    builtins.formatting.shfmt,
+    builtins.formatting.stylua,
+
+    -- Code actions
+    builtins.code_actions.eslint_d,
+}
+
+local M = {}
+
+M.setup = function()
+    null_ls.setup({
+        sources = sources,
+        on_attach = lsp_utils.on_attach,
+        debounce = 150
+    })
+end
+
+return M
