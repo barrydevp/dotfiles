06dcf95aa197a6c055b9dfc1bbfdfabecba8d82c
barrydevp <barrydevp@gmail.com>
2023-09-27 14:05:15 +0700

    nvim

--- .config/nvim/lua/core/default_config.lua
+++ .config/nvim/lua/core/default_config.lua
@@ -19,7 +19,7 @@ M.ui = {
   -- theme = "ashes",
   -- theme = "wombat",
   transparency = false,
-  lsp_semantic_tokens = false, -- needs nvim v0.9, just adds highlight groups for lsp semantic tokens
+  lsp_semantic_tokens = true, -- needs nvim v0.9, just adds highlight groups for lsp semantic tokens
 
   -- https://github.com/NvChad/base46/tree/v2.0/lua/base46/extended_integrations
   extended_integrations = {}, -- these aren't compiled by default, ex: "alpha", "notify"
