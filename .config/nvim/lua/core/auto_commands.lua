9e34a09f43e637cb76cd8351e061ca12cad0fe9e
barrydevp <barrydevp@gmail.com>
2024-03-14 16:36:04 -0400

    lskitty

--- .config/nvim/lua/core/auto_commands.lua
+++ .config/nvim/lua/core/auto_commands.lua
@@ -42,5 +42,13 @@ autocmd("FileType", {
 -- disable auto comment on the next line
 autocmd("FileType", {
   pattern = "*",
-  command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
+  callback = function()
+    -- if vim.bo.filetype == "javascript" then
+    --   return
+    -- end
+
+    vim.opt_local.formatoptions:remove { "r", "o", "c" }
+    -- do the rest of the callback
+  end,
+  -- command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
 })
--- .config/nvim/lua/plugins/configs/catppuccin.lua
+++ .config/nvim/lua/plugins/configs/catppuccin.lua
@@ -30,70 +30,74 @@ require("catppuccin").setup {
     operators = {},
     -- miscs = {}, -- Uncomment to turn off hard-coded styles
   },
-  custom_highlights = function(colors)
+  custom_highlights = function(C)
     return {
       -- WinBar = { bg = colors.mantle },
       -- WinBarNC = { bg = colors.mantle },
 
+      LineNrAbove = { fg = C.surface1 },
+      LineNrBelow = { fg = C.surface1 },
+      LineNr = { fg = C.lavender },
+
       -- Tabufline
       TblineFill = {
-        bg = colors.crust,
+        bg = C.crust,
       },
       TbLineBufOn = {
-        fg = colors.lavender,
-        bg = colors.crust,
+        fg = C.lavender,
+        bg = C.crust,
       },
       TbLineBufOff = {
-        fg = colors.surface1,
-        bg = colors.mantle,
+        fg = C.surface1,
+        bg = C.mantle,
       },
       TbLineBufOnModified = {
-        fg = colors.green,
-        bg = colors.crust,
+        fg = C.green,
+        bg = C.crust,
       },
       TbLineBufOffModified = {
-        fg = colors.red,
-        bg = colors.mantle,
+        fg = C.red,
+        bg = C.mantle,
       },
       TbLineBufOnClose = {
-        fg = colors.red,
-        bg = colors.crust,
+        fg = C.red,
+        bg = C.crust,
       },
       TbLineBufOffClose = {
-        fg = colors.surface1,
-        bg = colors.mantle,
+        fg = C.surface1,
+        bg = C.mantle,
       },
       TblineTabNewBtn = {
-        fg = colors.lavender,
-        bg = colors.overlay2,
+        fg = C.lavender,
+        bg = C.overlay2,
         bold = true,
       },
       TbLineTabOn = {
-        fg = colors.crust,
-        bg = colors.blue,
+        fg = C.crust,
+        bg = C.blue,
         bold = true,
       },
       TbLineTabOff = {
-        fg = colors.lavender,
-        bg = colors.overlay2,
+        fg = C.lavender,
+        bg = C.overlay2,
       },
       TbLineTabCloseBtn = {
-        fg = colors.crust,
-        bg = colors.blue,
+        fg = C.crust,
+        bg = C.blue,
       },
       TBTabTitle = {
-        fg = colors.crust,
-        bg = colors.lavender,
+        fg = C.crust,
+        bg = C.lavender,
       },
       TbLineThemeToggleBtn = {
         bold = true,
-        fg = colors.lavender,
-        bg = colors.overlay2,
+        fg = C.lavender,
+        bg = C.overlay2,
       },
       TbLineCloseAllBufsBtn = {
         bold = true,
-        bg = colors.red,
-        fg = colors.crust,
+        bg = C.red,
+        fg = C.crust,
       },
     }
   end,
