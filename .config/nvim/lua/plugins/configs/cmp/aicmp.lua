8a8f0de077255525c0d124dcda35d1a286342107
barrydevp <barrydevp@gmail.com>
2024-03-16 14:50:17 -0400

    nvim

---
+++ .config/nvim/lua/plugins/configs/cmp/aicmp.lua
@@ -0,0 +1,46 @@
+local config = require("core.utils").load_config()
+
+local M = {
+  default = {},
+  tabnine = {},
+  copilotvim = {},
+  copilot = {},
+}
+
+M.default.accept = function()
+  return false
+end
+
+M.tabnine.accept = function()
+  local completion = require("tabnine.completion")
+  local state = require("tabnine.state")
+
+  if state.completions_cache then
+    vim.schedule(completion.accept)
+    return true
+  end
+
+  return false
+end
+
+M.copilotvim.accept = function()
+  local cmd = vim.fn["copilot#Accept"]("\\<CR>")
+  if cmd ~= "\\<CR>" then
+    vim.api.nvim_feedkeys(cmd, "n", true)
+
+    return true
+  end
+
+  return false
+end
+
+M.copilot.accept = function()
+  return require("copilot.suggestion").accept()
+end
+
+-- set AI code generation completion
+M.accept = (function()
+  return (M[config.code.ai] or M.default)["accept"]
+end)()
+
+return M
---
+++ .config/nvim/lua/plugins/configs/copilotvim.lua
@@ -0,0 +1,5 @@
+vim.g.copilot_no_tab_map = true
+
+local M = {}
+
+return M
